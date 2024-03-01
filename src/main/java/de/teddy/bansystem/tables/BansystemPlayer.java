b3751953282cfbe2ec5d30928fde92d667a67e8d
Wetterquarz <cedric.lippke@hotmail.de>
2024-03-01 18:53:45 +0100

    Added cam_owner_uuid field to BansystemPlayer

--- src/main/java/de/teddy/bansystem/tables/BansystemPlayer.java
+++ src/main/java/de/teddy/bansystem/tables/BansystemPlayer.java
@@ -36,6 +36,9 @@ public class BansystemPlayer implements Serializable {
 	@Column(name = "last_login")
 	private Date lastLogin;
 
+	@Column(name = "cam_owner_uuid", nullable = false, unique = true, length = 36, columnDefinition = "varchar(36)")
+	private String camOwnerUuid;
+
 	public BansystemPlayer(){}
 
 	public BansystemPlayer(String uuid, Date firstLogin){
